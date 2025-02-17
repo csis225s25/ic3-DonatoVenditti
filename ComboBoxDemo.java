69af19a8c985063511f9c18ab630be52876fda8e
NZ <nj15zica@siena.edu>
2025-02-17 12:21:04 -0500

    reverted back to the original

--- ComboBoxDemo.java
+++ ComboBoxDemo.java
@@ -1,87 +1,30 @@
-/** Java Program to create a simple JComboBox
-*
-*  Modified from Geeks for Geeks user @andrew1234
-*  @author Ira Goldstein
 *  @version Spring 2010 
 */
 
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.FlowLayout;
-import java.awt.Color;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JComboBox;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
-class ComboBoxDemo implements ItemListener {
+class ComboBoxDemo extends JFrame implements ItemListener {
 
-	// frame
-	static JFrame frame;
+// frame
+static JFrame frame;
 
-	// label
-	static JLabel label, label1;
+// label
+static JLabel label, label1;
 
-	// combobox
-	static JComboBox cBox1;
+// combobox
+static JComboBox cBox1;
 
-	// main class
-	public static void main(String[] args)
-	{
-		// create a new frame
-		frame = new JFrame("Combo Box Demo");
-		
-        // tell the JFrame that when someone closes the
-        // window, the application should terminate
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+// main class
+public static void main(String[] args)
+{
+// create a new frame
+frame = new JFrame("Combo Box Demo");
 
-		// create a object
-		ComboBoxDemo s = new ComboBoxDemo();
+// tell the JFrame that when someone closes the
+// window, the application should terminate
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 
-		// set layout of frame
-		frame.setLayout(new FlowLayout());
-		
-		// array of string containing cities
-		String s1[] = { "Albany", "Schenectady", "Troy", "Glens Falls", "Catskill" };
-
-		// create checkbox
-        cBox1 = new JComboBox(s1);
-		
-		// add ItemListener
-		cBox1.addItemListener(s);
-
-		// create labels
-		label = new JLabel("select your city ");
-		label1 = new JLabel("Albany selected");
-
-		// set color of text
-		label.setForeground(Color.red);
-	    label1.setForeground(Color.blue);
-
-		// create a new panel
-		JPanel p = new JPanel();
-
-		p.add(label);
-
-		// add combobox to panel
-		p.add(cBox1);
-
-		p.add(label1);
-
-		// add panel to frame
-		frame.add(p);
-
-		// set the size of frame
-		frame.setSize(400, 300);
-
-		frame.setVisible(true);
-	}
-	public void itemStateChanged(ItemEvent e)
-	{
-		// if the state combobox is changed
-		if (e.getSource() == cBox1) {
-
-			label1.setText(cBox1.getSelectedItem() + " selected");
-		}
-	}
-}
+// create a object
+ComboBoxDemo s = new ComboBoxDemo();
